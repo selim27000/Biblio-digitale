3c90977f144b78ff8732db44e61ec7d4bd763f6d
selim27000 <selim27000@hotmail.com>
2022-07-31 23:24:29 +0200

    query - training

--- server.js
+++ server.js
@@ -2,19 +2,37 @@ const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
-const { MONGODB } = require('./config')
+const Post = require('./models/Post');
+const { MONGODB } = require('./config');
 
 const typeDefs = gql`
-type Query {
-    sayHi: String!
+type Post{
+    id: ID!
+    body: String!
+    createdAt : String!
+    username : String!
 }
-`
+
+type Query {
+    getPosts : [Post]
+}
+`;
 
 const resolvers = {
 Query : {
-    sayHi : ()=> 'Hello World'
+    async getPosts(){
+
+        try{
+const posts = await Post.find();
+return posts;
+        }catch (err){
+            throw new Error(err);
+        }
+
+    } 
     }
 }
+
 
 const server = new ApolloServer({
 typeDefs,
