9da6e4401a0f7f4cf78a846486e69a1679f7bd59
selim27000 <selim27000@hotmail.com>
2022-07-31 13:54:29 +0200

    config server + database

---
+++ server.js
@@ -0,0 +1,31 @@
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+
+const { MONGODB } = require('./config')
+
+const typeDefs = gql`
+type Query {
+    sayHi: String!
+}
+`
+
+const resolvers = {
+Query : {
+    sayHi : ()=> 'Hello World'
+    }
+}
+
+const server = new ApolloServer({
+typeDefs,
+resolvers
+})
+
+mongoose.connect(MONGODB, { useNewUrlParser : true })
+.then(()=>{
+    console.log('Mongodb connecté');
+    return server.listen({port : 5001});
+})
+.then( res => {
+    console.log(`serveur runing at ${res.url}`)
+})
