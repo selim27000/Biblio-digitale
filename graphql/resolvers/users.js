f7b3a4303a119532b32bf18ff8617886305673f5
selim27000 <selim27000@hotmail.com>
2022-08-01 12:11:05 +0200

    register - hash + token

---
+++ graphql/resolvers/users.js
@@ -0,0 +1,44 @@
+const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+
+const {SECRET_KEY}=require('../../config');
+
+module.exports = {
+
+    Mutation:{
+        async register(_, 
+            {registerInput : 
+            {username, email, password, confirmPassword }
+        }, 
+        context,
+        info)
+
+            {
+            //VALIDER LES DATA DE L'USER
+            //ETRE SUR QUE L'USER N'EXISTE PAS
+            //HASH DU MOT DE PASSE ET CREATION D'UN TOKEN
+            password = await bcrypt.hash(password,12);
+
+            const newUser = new User({
+
+                email,
+                username,
+                password,
+                createdAt: new Date().toISOString()
+            });
+            const res = await newUser.save();
+
+            const token = jwt.sign({
+                id:res.id,
+                email:res.email,
+                username:res.username
+            }, SECRET_KEY, { expiresIn: '1h'});
+            return {
+                ...res._doc,
+                id: res._id,
+                token
+            };
+        }
+    }
+}
