81d6014b19d4fc7d03ed415f18c3e129ee15825e
selim27000 <selim27000@hotmail.com>
2022-08-08 23:24:58 +0200

    delete post

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -7,7 +7,7 @@ module.exports = {
     Query : {
         async getPosts(){
             try{
-    const posts = await Post.find();
+    const posts = await Post.find().sort({createdAt : -1});
     return posts;
             }catch (err){
                 throw new Error(err);
@@ -38,6 +38,21 @@ module.exports = {
 const post = await newPost.save();
 
 return post;
-            }
+            },
+            async deletePost(_, { postId }, context) {
+              const user = checkAuth(context);
+        
+              try {
+                const post = await Post.findById(postId);
+                if (user.username === post.username) {
+                  await post.delete();
+                  return 'Post deleted successfully';
+                } else {
+                  throw new AuthenticationError('Action not allowed');
+                }
+              } catch (err) {
+                throw new Error(err);
+              }
+            },
         }
 };
